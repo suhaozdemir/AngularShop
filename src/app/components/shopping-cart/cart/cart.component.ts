8f5a204fe9d0d37d5a57892d6f85de497c1f6daa
suhaozdemir <77856467+suhaozdemir@users.noreply.github.com>
2021-12-13 17:28:16 +0300

    Updated Shopping Cart

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  //Sepetteki urunler
+  cartItems = [
+     { id: 1, proudctId: 1, productName: 'Test 1', qty: 4, price: 100 },
+     { id: 2, proudctId: 3, productName: 'Test 3', qty: 5, price: 50 },
+     { id: 3, proudctId: 2, productName: 'Test 2', qty: 3, price: 150 },
+     { id: 4, proudctId: 4, productName: 'Test 4', qty: 2, price: 200 },
+  ];
+
+  cartTotal = 0;
+
   constructor() { }
 
+  //Init durumunda sepetteki urunlerin toplam fiyatini gostermek icin
   ngOnInit(): void {
+    this.cartItems.forEach(item =>{
+      this.cartTotal += (item.qty * item.price)
+    })
   }
 
 }
